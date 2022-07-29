8cb43d71b4074112cbbf0abf7901febe47fca09c
Nikita Singh <nrsingh@fb.com>
2022-07-29 09:43:04 -0700

    pods removed

--- Pods/Target Support Files/Pods-meta_capstone-meta_capstoneUITests/Pods-meta_capstone-meta_capstoneUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_meta_capstone_meta_capstoneUITests : NSObject
-@end
-@implementation PodsDummy_Pods_meta_capstone_meta_capstoneUITests
-@end
--- Pods/Target Support Files/Pods-meta_capstoneTests/Pods-meta_capstoneTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_meta_capstoneTests : NSObject
-@end
-@implementation PodsDummy_Pods_meta_capstoneTests
-@end
